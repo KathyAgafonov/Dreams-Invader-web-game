4e479afc722c638586cc40619f6229aef9b5178a
KathyAgafonov <kathyagafonov@gmail.com>
2023-04-27 19:59:28 +0300

    test

--- js/screens/settings.js
+++ js/screens/settings.js
@@ -63,7 +63,8 @@ document.addEventListener('keyup', (event) => {
   // If the user presses PLAY when the shootkey input box it will pop up an alert and not let him start playing
   function playButtonClicked() {
     var shootKeyInput = document.getElementsByName('shootKey')[0];
-    
+    console.log(shootKeyInput)
+    welcomePage_show()
     // if (shootKeyInput.value.trim() == '') {
     //   alert('Please enter a value for the shootKey input.');
     //   return false;
